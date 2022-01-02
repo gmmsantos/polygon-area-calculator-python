07108bf57dbf74b2c6aec982051525907fb69223
Gabriel Santos <gabriel.santos@wildlifestudios.com>
2022-01-02 17:24:24 -0300

    Modified the user interation and main.py

--- polygon-area-calculator/main.py
+++ polygon-area-calculator/main.py
@@ -1,19 +1,64 @@
 from common import rectangle, square
 from unittest import main
 
-rect = rectangle.Rectangle(10, 5)
-print(rect.get_area())
-rect.set_height(3)
-print(rect.get_perimeter())
-print(rect)
-print(rect.get_picture())
+print("Welcome to Polygon Calculator")
 
-sq = square.Square(9)
-print(sq.get_area())
-sq.set_side(4)
-print(sq.get_diagonal())
-print(sq)
-print(sq.get_picture())
+width = int(input("Please inform the WIDTH of the polygon: "))
+height = int(input("Please inform the HEIGHT of the polygon: "))
+
+while True:
+
+    action = input(
+        "Please, Choose one option: "
+        + "\n"
+        + "[1] - Set a new Width"
+        + "\n"
+        + "[2] - Set a new Height"
+        + "\n"
+        + "[3] - Calculate Area"
+        + "\n"
+        + "[4] - Calculate Perimeter"
+        + "\n"
+        + "[5] - Calculate Diagonal"
+        + "\n"
+        + "[6] - Display Polygon Picture"
+        + "\n"
+    )
+
+    if width == height:
+        sq = square.Square(width)
+
+    rect = rectangle.Rectangle(width, height)
+
+    if action == "1":
+        w = int(input("Set the new value: "))
+        rect.set_width(w)
+        print(rect)
+    elif action == "2":
+        h = int(input("Set the new value: "))
+        rect.set_height(h)
+        print(rect)
+    elif action == "3":
+        print("\n")
+        print(rect.get_area())
+    elif action == "4":
+        print("\n")
+        print(rect.get_perimeter())
+    elif action == "5":
+        print("\n")
+        print(rect.get_diagonal())
+    elif action == "6":
+        print("\n")
+        print(rect.get_picture())
+
+    new_calc = input("Would you like to choose another action? Enter 'y' or 'n' ")
+
+    if new_calc[0].lower() == "y":
+        playing = True
+        continue
+    else:
+        print("Thanks!")
+        break
 
 # Run unit tests automatically
 main(
